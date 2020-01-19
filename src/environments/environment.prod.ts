a714c60cb4dc7371469e98f6982777cdc49c7395
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2020-01-19 15:43:27 -0500

    Creacion de una aplicación en Ionic. Misma aplicacion cuenta con un crud y un splash screen animado

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAO_tC2zlA4isyLHRL0bLCDw48TQBSLMPU",
+    authDomain: "calificaciones-8a002.firebaseapp.com",
+    databaseURL: "https://calificaciones-8a002.firebaseio.com",
+    projectId: "calificaciones-8a002",
+    storageBucket: "calificaciones-8a002.appspot.com",
+    messagingSenderId: "539660432163",
+    appId: "1:539660432163:web:4ed5ec1a7627c22d521296"
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAO_tC2zlA4isyLHRL0bLCDw48TQBSLMPU",
+    authDomain: "calificaciones-8a002.firebaseapp.com",
+    databaseURL: "https://calificaciones-8a002.firebaseio.com",
+    projectId: "calificaciones-8a002",
+    storageBucket: "calificaciones-8a002.appspot.com",
+    messagingSenderId: "539660432163",
+    appId: "1:539660432163:web:4ed5ec1a7627c22d521296"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
